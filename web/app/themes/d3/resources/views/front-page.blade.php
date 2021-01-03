13672154a56d37fffc9aab30eed9b29ccf148ba2
aitor <aitor@e451.net>
2021-01-03 11:20:34 +0100

    front page hero tags

--- web/app/themes/d3/resources/views/front-page.blade.php
+++ web/app/themes/d3/resources/views/front-page.blade.php
@@ -3,6 +3,49 @@
 @section('content')
 
 
-<div id="contenedor" class="escamas"></div>
+<div id="container-fluid" class="hero">
+  <div class="row">
+    <div class="col-7 d-flex flex-column">
+      <div class="filtros d-flex">
+        <a href="#" class="escama wow">
+          @svg("images.escama")
+        </a>
+        <a href="#" class="escama overwatch">
+          @svg("images.escama")
+        </a>
+        <a href="#" class="escama heroes">
+          @svg("images.escama")
+        </a>
+        <a href="#" class="escama diablo">
+          @svg("images.escama")
+        </a>
+        <a href="#" class="escama hearthstone">
+          @svg("images.escama")
+        </a>
+        <a href="#" class="escama starcraft">
+          @svg("images.escama")
+        </a>
+      </div>
+      <div class="tags d-flex flex-wrap">
+        <a href="#" class="tag wow">Hunter</a>
+        <a href="#" class="tag overwatch">Sacerdote</a>
+        <a href="#" class="tag diablo">Druid</a>
+        <a href="#" class="tag starcraft">Mago</a>
+        <a href="#" class="tag hearthstone">Monk</a>
+        <a href="#" class="tag heroes">Demon Hunter</a>
+        <a href="#" class="tag diablo">Arquero</a>
+        <a href="#" class="tag overwatch">Chamán</a>
+        <a href="#" class="tag wow">Death Knight</a>
+        <a href="#" class="tag diablo">Tortuga</a>
+        <a href="#" class="tag heroes">Sicario</a>
+        <a href="#" class="tag wow">Buscador de oro</a>
+        <a href="#" class="tag overwatch">Pícaro</a>
+        <a href="#" class="tag wow">arquero</a>
+        <a href="#" class="tag overwatch">Escupefuego</a>
+        <a href="#" class="tag heroes">Espantapájaros</a>
+      </div>
+    </div>
+  </div>
+</div>
 
 @endsection
